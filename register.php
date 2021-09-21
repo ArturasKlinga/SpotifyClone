b1997d960726f568990059f41ab0e1d33459645c
Arturas Klinga <arturas.klinga@panko.lt>
2021-09-21 18:58:39 +0300

    Inserting user details in database.

--- register.php
+++ register.php
@@ -1,7 +1,8 @@
 <?php
+    include("includes/config.php");
     include("includes/classes/Account.php");
     include("includes/classes/Constants.php");
-    $account = new Account();
+    $account = new Account($con);
     include("includes/handlers/register-handler.php");
     include("includes/handlers/login-handler.php");
 
@@ -35,23 +36,23 @@
             <form id="registerForm" action="register.php" method="POST">
                 <h2>Create your free account</h2>
                 <p>
-                    <?php echo $account->getError("Your username must be between 5 and 25 characters."); ?>
+                    <?php echo $account->getError(Constants::$usernameCharacters); ?>
                     <label for="username">Username</label>
                     <input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
                 </p>
                 <p>
-                    <?php echo $account->getError("Your first name must be between 2 and 25 characters."); ?>
+                    <?php echo $account->getError(Constants::$firstNameCharacters); ?>
                     <label for="firstName">First name</label>
                     <input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstName') ?>" required>
                 </p>
                 <p>
-                    <?php echo $account->getError("Your last name must be between 2 and 25 characters."); ?>
+                    <?php echo $account->getError(Constants::$lastNameCharacters); ?>
                     <label for="lastName">Last name</label>
                     <input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastName') ?>" required>
                 </p>
                 <p>
-                    <?php echo $account->getError("Your emails don't match."); ?>
-                    <?php echo $account->getError("Email is invalid."); ?>
+                    <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+                    <?php echo $account->getError(Constants::$emailInvalid); ?>
                     <label for="email">Email</label>
                     <input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
                 </p>
@@ -60,9 +61,9 @@
                     <input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
                 </p>
                 <p>
-                    <?php echo $account->getError("Your passwords don't match."); ?>
-                    <?php echo $account->getError("Your password can only contain numbers and letters."); ?>
-                    <?php echo $account->getError("Your password must be between 5 and 30 characters."); ?>
+                    <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
+                    <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+                    <?php echo $account->getError(Constants::$passwordCharacters); ?>
                     <label for="password">Password</label>
                     <input id="password" name="password" type="password" placeholder="Your password" value="<?php getInputValue('password') ?>" required>
                 </p>
