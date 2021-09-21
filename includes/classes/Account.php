d5f87b2d17270d31b1a0d978121485edd72760a9
Arturas Klinga <arturas.klinga@panko.lt>
2021-09-21 12:07:54 +0300

    Outputting error messages.

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -13,6 +13,21 @@
             $this->validateLastName($ln);
             $this->validateEmails($em, $em2);
             $this->validatePasswords($pw, $pw2);
+
+            if (empty($this->errorArray)) {
+                //Insert into db.
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public function getError($error) {
+            if (!in_array($error, $this->errorArray)) {
+                $error = "";
+            }
+            return "<span class='errorMessage'>$error</span>";
         }
 
         private function validateUsername($un) {
