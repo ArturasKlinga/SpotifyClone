2dc78129001e7ea5a08c3747d8f9d0ad60e0804a
Arturas Klinga <arturas.klinga@panko.lt>
2021-09-23 16:26:57 +0300

    Remembering username after failed log in, error messages styling.

--- register.php
+++ register.php
@@ -51,7 +51,7 @@
                         <p>
                             <?php echo $account->getError(Constants::$loginFailed); ?>
                             <label for="loginUsername">Username</label>
-                            <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>
+                            <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
                         </p>
                         <p>
                             <label for="loginPassword">Password</label>
