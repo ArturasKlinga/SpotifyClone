a44d542d3acf97e3d1e60a26657af3f003395b13
Arturas Klinga <arturas.klinga@panko.lt>
2021-09-27 16:33:52 +0300

    Playing and pausing the song via buttons.

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let currentPlaylist = [];
+let audioElement;
+
+function Audio() {
+    this.audio = document.createElement("audio");
+    this.setTrack = function(src) {
+        this.audio.src = src;
+    }
+    this.play = function() {
+        this.audio.play();
+    }
+
+    this.pause = function() {
+        this.audio.pause();
+    }
+}
--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -1,3 +1,39 @@
+<?php
+    $songQuery = mysqli_query($con, "SELECT id FROM songs ORDER BY RAND() LIMIT 10");
+    $resultArray = array();
+    while ($row = mysqli_fetch_array($songQuery)) {
+        array_push($resultArray, $row['id']);
+    }
+    $jsonArray = json_encode($resultArray);
+?>
+
+<script>
+    $(document).ready(function() {
+        currentPlaylist = <?php echo $jsonArray; ?>;
+        audioElement = new Audio();
+        setTrack(currentPlaylist[0], currentPlaylist, false)
+    });
+
+    function setTrack(trackId, newPlaylist, play) {
+        audioElement.setTrack("assets/music/bensound-acousticbreeze.mp3");
+        if (play) {
+            audioElement.play();
+        }
+    }
+
+    function playSong() {
+        $(".controlButton.play").hide()
+        $(".controlButton.pause").show()
+        audioElement.play();
+    }
+
+    function pauseSong() {
+        $(".controlButton.play").show()
+        $(".controlButton.pause").hide()
+        audioElement.pause();
+    }
+</script>
+
 <div id="nowPlayingBarContainer">
     <div id="nowPlayingBar">
         <div id="nowPlayingLeft">
@@ -24,10 +60,10 @@
                     <button class="controlButton previous" title="Previous button">
                         <img src="assets/images/icons/previous.png" alt="Previous">
                     </button>
-                    <button class="controlButton play" title="Play button">
+                    <button class="controlButton play" title="Play button" onclick="playSong()">
                         <img src="assets/images/icons/play.png" alt="Play">
                     </button>
-                    <button class="controlButton pause" title="Pause button" style="display: none;">
+                    <button class="controlButton pause" title="Pause button" style="display: none;" onclick="pauseSong()">
                         <img src="assets/images/icons/pause.png" alt="Pause">
                     </button>
                     <button class="controlButton next" title="Next button">
