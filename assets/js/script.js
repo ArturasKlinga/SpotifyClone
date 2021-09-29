622ea0184a07047686a5ec7bc004934fc4e6f4d2
Arturas Klinga <arturas.klinga@panko.lt>
2021-09-29 18:39:59 +0300

    Made volume bar draggable.

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,12 @@ function updateTimeProgressBar(audio) {
 
 }
 
+function updateVolumeProgressBar(audio) {
+    let volume = audio.volume * 100;
+    $(".volumeBar .progress").css("width", volume + "%");
+
+}
+
 function Audio() {
     this.currentlyPlaying;
     this.audio = document.createElement("audio");
@@ -29,6 +35,9 @@ function Audio() {
         if (this.duration) {
             updateTimeProgressBar(this);
         }
+    });
+    this.audio.addEventListener("volumechange", function() {
+        updateVolumeProgressBar(this);
     });
     this.setTrack = function(track) {
         this.currentlyPlaying = track;
