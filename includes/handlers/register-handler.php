d07abd75ccd152e7eb267c1e0a6c58c3d32e406e
Arturas Klinga <arturas.klinga@panko.lt>
2021-10-25 22:28:28 +0300

    Fixed issue where first email letter is always uppercase.

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -5,7 +5,7 @@
         return $inputText;
     }
 
-    function sanitizeFormUsername($inputText) {
+    function sanitizeFormUsernameAndEmail($inputText) {
         $inputText = strip_tags($inputText);
         $inputText = str_replace(" ", "", $inputText);
         return $inputText;
@@ -20,11 +20,11 @@
 
 
     if (isset($_POST['registerButton'])) {
-        $username = sanitizeFormUsername($_POST['username']);
+        $username = sanitizeFormUsernameAndEmail($_POST['username']);
         $firstName = sanitizeFormString($_POST['firstName']);
         $lastName = sanitizeFormString($_POST['lastName']);
-        $email = sanitizeFormString($_POST['email']);
-        $email2 = sanitizeFormString($_POST['email2']);
+        $email = sanitizeFormUsernameAndEmail($_POST['email']);
+        $email2 = sanitizeFormUsernameAndEmail($_POST['email2']);
         $password = sanitizeFormPassword($_POST['password']);
         $password2 = sanitizeFormPassword($_POST['password2']);
         $wasSuccessful = $account->register($username, $firstName, $lastName, $email, $email2, $password, $password2);
